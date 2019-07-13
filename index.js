f8f73075e982988f2795858d51fe3a6db66cd059
Natthapong Muensri <59143409@g.cmru.ac.th>
2019-07-13 14:13:45 +0700

    มtest

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ const port = 4000
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/about', (req, res) => res.send('โบเอง'))
 app.get('/contact', (req, res) => res.send('FB : Natthapong Muensri tel : 0992929062'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
